4aac65d032fcb0218efdb1cc9c5d6238f6002e93
Abhishek Raut <102864290+abhishekraut1@users.noreply.github.com>
2023-09-01 09:10:42 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,14 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div>
-      NoteIt is a modern and intuitive note-saving application that empowers users to efficiently organize, manage, and secure
-their important notes and thoughts. NoteIt offers a seamless and secure user experience that prioritizes data privacy
-and user convenience.
-NoteIt stands as a testament to the capabilities of the React ecosystem, paired with powerful backend technologies like
-Node.js, Express.js, and MongoDB. The integration of JWT and Bcrypt ensures the highest level of security for user
-accounts and data, giving users peace of mind while using the app.
-    <h5 className='my-4 text-center '>Made with <span className='.text-danger'>&#10084;</span>  by Abhishek Raut</h5>
+      <div >
+        <li>NoteIt is a modern and intuitive note-saving application that empowers users to efficiently organize, manage, and secure their important notes and </li>
+        <li> thoughts. NoteIt offers a seamless and secure user experience that prioritizes data privacy and user convenience.NoteIt stands as a testament to the</li>
+        <li>capabilities of the React ecosystem, paired with powerful backend technologies like Node.js, Express.js, and MongoDB. The integration of JWT and Bcrypt</li>
+        <li>ensures the highest level of security for user accounts and data, giving users peace of mind while using the app.</li>
+      </div>
+
+      <h5 className='mt-5 text-center '>Made with <span className='.text-danger'>&#10084;</span>  by Abhishek Raut</h5>
     </div>
   )
 }
